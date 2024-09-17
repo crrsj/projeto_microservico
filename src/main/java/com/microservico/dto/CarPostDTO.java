87ba1002c7449f50b2e96fa18e7e3b680cd21fff
Carlos Roberto <crrsj1@gmail.com>
2024-09-17 08:34:59 -0300

    Ajuste no DTO OwnerPostDTO

--- src/main/java/com/microservico/dto/CarPostDTO.java
+++ src/main/java/com/microservico/dto/CarPostDTO.java
@@ -1,5 +1,29 @@
 package com.microservico.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonInclude
+@Builder
 public class CarPostDTO {
 
+	private String model;
+	private String brand;
+	private String price;
+	private String description;
+	private String engineVersion;
+	private String city;
+	private String createdDate;
+	private Long ownerId;
+	private String counterName;
+	private String counterType;
+	private String contact;
+	
 }
--- src/main/java/com/microservico/dto/OwnerPostDto.java
+++ src/main/java/com/microservico/dto/OwnerPostDto.java
@@ -1,5 +1,20 @@
 package com.microservico.dto;
 
-public class OwnerPostDto {
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonInclude
+@Builder
+public class OwnerPostDTO {
+
+	private String name;
+	private String type;
+	private String contactNumber;
 }
